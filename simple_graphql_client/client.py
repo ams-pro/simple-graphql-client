c509dc20fba45462fc601c2953c5b3b64fed77e6
fabianriewe <fabianriewe00@gmail.com>
2020-02-15 01:08:35 +0100

    feat: improved more dynamic map building

--- simple_graphql_client/client.py
+++ simple_graphql_client/client.py
@@ -1,6 +1,8 @@
 import json
 
 import requests
+from graphql import parse
+from graphql.language.ast import ListType
 
 SUBDIR = None
 GRAPHQL_FILE_PATH = ''
@@ -40,9 +42,29 @@ class GraphQLClient:
 
         # Todo build file map more dynamic
         _map = {}
-        for file, i in zip(files, range(len(files))):
-            _map[file[0]] = ['variables.file.{}'.format(i)]
 
+        upload_fields = self._get_upload_fields(query)
+
+        for (field_name, is_list) in upload_fields:
+            print(field_name, is_list)
+
+            if is_list:
+
+                assert len(variables[field_name]) == len(files), \
+                    "Not enough values for either 'files' or 'files' in variables"
+
+                for file, i in zip(files, range(len(files))):
+                    # validate variables
+                    assert variables[field_name][i] is None, "No null value for {} at index {}".format(field_name, i)
+
+                    _map[file[0]] = ['variables.{}.{}'.format(field_name, i)]
+            else:
+                # validate variables
+                assert variables[field_name] is None, "No null value for {}".format(field_name)
+                files = [files]
+                _map["0"] = ['variables.{}'.format(field_name)]
+
+        print(_map)
         payload = {
             'operations': json.dumps(_operations),
             'map': json.dumps(_map)
@@ -50,6 +72,32 @@ class GraphQLClient:
 
         response_json = self._make_request(query=query, payload=payload, headers=headers, files=files)
         return response_json
+
+    def _get_upload_fields(self, query):
+
+        upload_fields = []
+
+        parsed = parse(query)
+        variables = parsed.definitions[0].variable_definitions
+        for variable in variables:
+            # get type depth
+            is_list = False
+            _type = variable.type
+            while True:
+                if isinstance(_type, ListType) and not is_list:
+                    is_list = True
+                try:
+                    new_type = _type.type
+                except AttributeError:
+                    break
+                _type = new_type
+
+            if _type.name.value == "Upload":
+                variable_name = variable.variable.name.value
+                upload_fields.append((variable_name, is_list))
+            else:
+                continue
+        return upload_fields
 
     def _make_request(self, query: str, headers: dict = None, payload: dict = None, json: dict = None,
                       files: list = None):
